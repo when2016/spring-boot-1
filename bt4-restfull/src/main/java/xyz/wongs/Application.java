3004717524300b8401a1edd9f9853cfecd1431d0
WCNGS <WCNGS@QQ.COM>
2018-11-07 10:10:24 +0800

    添加监控

--- bt4-restfull/src/main/java/xyz/wongs/Application.java
+++ bt4-restfull/src/main/java/xyz/wongs/Application.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class Application {
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
